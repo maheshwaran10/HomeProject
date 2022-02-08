4adf9860c116c7637b1cfba0eac8aa33e5a53734
Maheshwaran <maheshwaran08062000@gmail.co>
2022-02-08 06:56:43 +0530

    commit message

---
+++ app/src/main/java/com/example/homeproject/api/ApiActivity.kt
@@ -0,0 +1,12 @@
+package com.example.homeproject.api
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.homeproject.R
+
+class ApiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_api)
+    }
+}
